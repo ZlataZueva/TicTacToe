c8f3f6a9408c86a4c1c257c0fe11ff1cb75aca98
Zlata <zlatka.zueva@gmail.com>
2019-07-26 18:35:08 +0300

    Create player and player factory

---
+++ src/TicTacToe.Console/Interfaces/IPlayerFactory.cs
@@ -0,0 +1,10 @@
+﻿using iTechArt.TicTacToe.Foundation.Figures;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Console.Interfaces
+{
+    public interface IPlayerFactory
+    {
+        IPlayer CreatePlayer(string firstName, string lastName, FigureType figureType);
+    }
+}
---
+++ src/TicTacToe.Console/Players/PlayerFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using iTechArt.TicTacToe.Foundation.Figures;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+using TicTacToe.Console.Interfaces;
+
+namespace TicTacToe.Console.Players
+{
+    public class PlayerFactory : IPlayerFactory
+    {
+        public IPlayer CreatePlayer(string firstName, string lastName, FigureType figureType)
+        {
+            if (firstName == string.Empty || lastName == string.Empty)
+            {
+                throw new ArgumentException("Player's first and last names should not be empty");
+            }
+            if (firstName.ToCharArray().Any(char.IsDigit) || lastName.ToCharArray().Any(char.IsDigit))
+            {
+                throw new ArgumentException("Player's first and last names should not contain digits");
+            }
+
+            return new Player(firstName, lastName, figureType);
+        }
+    }
+}
