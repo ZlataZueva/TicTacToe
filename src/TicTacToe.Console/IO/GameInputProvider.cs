8f89831699be60509f847a16808d08a313ecd66b
Zlata <zlatka.zueva@gmail.com>
2019-07-29 16:31:46 +0300

    Implement IGameInputProvider interface

---
+++ src/TicTacToe.Console/IO/GameInputProvider.cs
@@ -0,0 +1,28 @@
+﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+using TicTacToe.Console.Interfaces;
+
+namespace TicTacToe.Console.IO
+{
+    public class GameInputProvider : IGameInputProvider
+    {
+        private readonly IConsole _console;
+        private readonly IConsoleInputProvider _consoleInput;
+
+
+        public GameInputProvider(IConsoleInputProvider consoleInput, IConsole console)
+        {
+            _console = console;
+            _consoleInput = consoleInput;
+        }
+
+
+        public (int row, int column) GetPositionToMakeMove(IPlayer player)
+        {
+            _console.WriteLine($"{player.FirstName} {player.LastName}, please, choose where to put figure:");
+            var row = _consoleInput.GetInt("Row:") - 1;
+            var column = _consoleInput.GetInt("Column:") - 1;
+
+            return (row, column);
+        }
+    }
+}
