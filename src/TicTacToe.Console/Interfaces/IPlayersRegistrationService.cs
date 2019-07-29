f1f41e391fbbe2d19880d4e27b885c555a0d1d6e
Zlata <zlatka.zueva@gmail.com>
2019-07-29 14:25:55 +0300

    Pass available figures as method parameter

--- src/TicTacToe.Console/Interfaces/IPlayersRegistrationService.cs
+++ src/TicTacToe.Console/Interfaces/IPlayersRegistrationService.cs
@@ -1,9 +1,11 @@
-﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+﻿using System.Collections.Generic;
+using iTechArt.TicTacToe.Foundation.Figures;
+using iTechArt.TicTacToe.Foundation.Interfaces;
 
 namespace TicTacToe.Console.Interfaces
 {
     public interface IPlayersRegistrationService
     {
-        IPlayer Register();
+        IPlayer Register(IList<FigureType> availableFigureTypes);
     }
 }
--- src/TicTacToe.Console/Players/PlayerRegistrationService.cs
+++ src/TicTacToe.Console/Players/PlayerRegistrationService.cs
@@ -14,54 +14,49 @@ namespace TicTacToe.Console.Players
         private readonly IConsole _console;
         private readonly IConsoleInputProvider _consoleInput;
 
-        private readonly IList<FigureType> _availableFigureTypes;
-
 
         public PlayerRegistrationService(IPlayerFactory playerFactory, IConsole console, IConsoleInputProvider consoleInput)
         {
             _playerFactory = playerFactory;
             _console = console;
             _consoleInput = consoleInput;
-
-            _availableFigureTypes = Enum.GetValues(typeof(FigureType)).Cast<FigureType>().ToList();
         }
 
 
-        public IPlayer Register()
+        public IPlayer Register(IList<FigureType> availableFigureTypes)
         {
             var firstName = _consoleInput.GetString("Please, enter player's first name:");
             var lastName = _consoleInput.GetString("Please, enter player's last name:");
-            var figureType = ChooseFigure();
+            var figureType = ChooseFigure(availableFigureTypes);
             var player = _playerFactory.CreatePlayer(firstName, lastName, figureType);
-            _availableFigureTypes.Remove(figureType);
             _console.WriteLine($"Registered player: {firstName} {lastName} with figure - {figureType}.");
 
             return player;
         }
 
 
-        private FigureType ChooseFigure()
+        private FigureType ChooseFigure(IList<FigureType> availableFigureTypes)
         {
-            if (!_availableFigureTypes.Any())
+            if (!availableFigureTypes.Any())
             {
                 throw new ArgumentException("No more figures left");
             }
             int chosenFigureNumber;
-            if (_availableFigureTypes.Count == 1)
+            if (availableFigureTypes.Count == 1)
             {
                 chosenFigureNumber = 1;
             }
             else
             {
                 _console.WriteLine("Available figures:");
-                _availableFigureTypes.ForEach(figure => _console.WriteLine($"{_availableFigureTypes.IndexOf(figure)+1}. {figure}"));
+                availableFigureTypes.ForEach(figure => _console.WriteLine($"{availableFigureTypes.IndexOf(figure)+1}. {figure}"));
                 do
                 {
                     chosenFigureNumber = _consoleInput.GetInt("Please, enter figure's number:");
-                } while (chosenFigureNumber <= 0 || chosenFigureNumber > _availableFigureTypes.Count);
+                } while (chosenFigureNumber <= 0 || chosenFigureNumber > availableFigureTypes.Count);
             }
 
-            return _availableFigureTypes[chosenFigureNumber - 1];
+            return availableFigureTypes[chosenFigureNumber - 1];
         }
     }
 }
