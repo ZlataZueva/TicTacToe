d6d8a78cd613cfb73b62c34ee55c146f77a1768d
Zlata <zlatka.zueva@gmail.com>
2019-07-31 12:28:55 +0300

    Bootstrap

--- src/iTechArt.TicTacToe/Program.cs
+++ src/iTechArt.TicTacToe/Program.cs
@@ -1,27 +1,51 @@
 ﻿using iTechArt.TicTacToe.Foundation.Board;
 using iTechArt.TicTacToe.Foundation.Cell;
 using iTechArt.TicTacToe.Foundation.Figures;
+using iTechArt.TicTacToe.Foundation.Game;
+using iTechArt.TicTacToe.Foundation.WinningStates;
+using TicTacToe.Console.GameConfiguration;
+using TicTacToe.Console.Interfaces;
 using TicTacToe.Console.IO;
+using TicTacToe.Console.Players;
 using Console = TicTacToe.Console.IO.Console;
 
 namespace iTechArt.TicTacToe
 {
     class Program
     {
+        private static IGameOutputProvider _gameOutputProvider;
+
         static void Main(string[] args)
         {
-            var board = new Board(3, new CellFactory(), new FigureFactory());
-            var gameOutput = new GameOutputProvider(new Console());
-            gameOutput.ShowBoard(board);
-            board.PlaceFigure(1, 1, FigureType.Circle);
-            gameOutput.ShowBoard(board);
-            board.PlaceFigure(1, 2, FigureType.Cross);
-            gameOutput.ShowBoard(board);
-            board.PlaceFigure(2, 2, FigureType.Circle);
-            gameOutput.ShowBoard(board);
-            board.PlaceFigure(3, 1, FigureType.Cross);
-            gameOutput.ShowBoard(board);
-            System.Console.ReadLine();
+            var console = new Console();
+            var consoleInputProvider = new ConsoleInputProvider(console);
+            var configurationService = new GameConfigurationService(
+                new GameConfigurationFactory(),
+                new PlayerRegistrationService(new PlayerFactory(), console, consoleInputProvider),
+                console,
+                consoleInputProvider);
+            var gameFactory = new GameFactory(
+                new BoardFactory(new CellFactory(), new FigureFactory()),
+                new WinningStatesFactory(), 
+                new GameInputProvider(consoleInputProvider, console));
+            _gameOutputProvider = new GameOutputProvider(console);
+
+            var configuration = configurationService.CreateGameConfiguration();
+            var game = gameFactory.CreateGame(configuration);
+            game.StepCompleted += OnStepCompleted;
+            game.GameFinished += OnGameFinished;
+            game.Run();
+        }
+
+
+        private static void OnStepCompleted(object sender, StepCompletedEventArgs e)
+        {
+            _gameOutputProvider.ShowStepResult(e.Result);
+        }
+
+        private static void OnGameFinished(object sender, GameFinishedEventArgs e)
+        {
+            _gameOutputProvider.ShowGameResult(e.Result);
         }
     }
 }
