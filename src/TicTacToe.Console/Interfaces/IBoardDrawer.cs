308330e745fb1798f7beeaab7610a4b1c4ce5675
Zlata <zlatka.zueva@gmail.com>
2019-07-31 14:15:30 +0300

    Move board drawing to separate class

---
+++ src/TicTacToe.Console/Interfaces/IBoardDrawer.cs
@@ -0,0 +1,9 @@
+﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace TicTacToe.Console.Interfaces
+{
+    public interface IBoardDrawer
+    {
+        void ShowBoard(IBoard board);
+    }
+}
