f5a0be8b1c2f8dca63edab54af7bd9b543961a64
Zlata <zlatka.zueva@gmail.com>
2019-08-01 16:47:29 +0300

    Remove drawing tests

--- src/iTechArt.TicTacToe/Program.cs
+++ src/iTechArt.TicTacToe/Program.cs
@@ -1,7 +1,8 @@
-﻿using iTechArt.TicTacToe.Console.Drawers;
-using iTechArt.TicTacToe.Foundation.Board;
-using iTechArt.TicTacToe.Foundation.Cell;
-using iTechArt.TicTacToe.Foundation.Figures;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace iTechArt.TicTacToe
 {
@@ -9,18 +10,6 @@ namespace iTechArt.TicTacToe
     {
         static void Main(string[] args)
         {
-            var board = new Board(3, new CellFactory(), new FigureFactory());
-            var boardDrawer = new BoardDrawer(new Console.IO.Console());
-            boardDrawer.DrawBoard(board);
-            board.PlaceFigure(1, 1, FigureType.Circle);
-            boardDrawer.DrawBoard(board);
-            board.PlaceFigure(1, 2, FigureType.Cross);
-            boardDrawer.DrawBoard(board);
-            board.PlaceFigure(2, 2, FigureType.Circle);
-            boardDrawer.DrawBoard(board);
-            board.PlaceFigure(3, 1, FigureType.Cross);
-            boardDrawer.DrawBoard(board);
-            System.Console.ReadLine();
         }
     }
 }
